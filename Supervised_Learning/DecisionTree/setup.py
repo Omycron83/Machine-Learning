49d05a9f5b390f0c9d70c86f9a6d4363e25b61c2
Damian <damian.grunert@gmx.de>
2023-08-02 17:28:40 +0200

    Apparently one should have seperate setup files

---
+++ Supervised_Learning/DecisionTree/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="DecisionTree",
+      version="0.1",
+      description= "Package implementing various Decision-Tree-Algorithms given reasonable data.",
+      packages=[r"D:\Damian\PC\Python\ML\Supervised_Learning\DecisionTree"],
+      author= "Damian G."
+)
+
