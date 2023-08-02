a40c96582184763cd9e84e0cd4bf6df50b2490d4
Damian <damian.grunert@gmx.de>
2023-08-02 17:28:40 +0200

    Apparently one should have seperate setup files

---
+++ RL/Blueprints/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="RL",
+      version="0.1",
+      description= "Package implementing various RL-Algorithms that can be applied to all environments with certain methods.",
+      packages=[r"D:\Damian\PC\Python\ML\RL\Blueprints"],
+      author= "Damian G."
+      )
---
+++ Supervised_Learning/NN_blueprint/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="NN",
+      version="0.1",
+      description= "Package implementing various Neural-Network types given reasonable data.",
+      packages=[r"D:\Damian\PC\Python\ML\Supervised_Learning\NN_blueprint"],
+      author= "Damian G."
+      )
