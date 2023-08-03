09d518378d1c3e1ae47c517ce06ea7465575843c
Damian <damian.grunert@gmx.de>
2023-08-03 18:57:44 +0200

    Changed stuff

--- BasicAlgorithms/LinearRegression/LinRegr.py
+++ BasicAlgorithms/LinearRegression/LinRegr.py
@@ -1,16 +1,18 @@
 import numpy as np
 class linear_regression:
-    def __init__(self, dim, _lambda = 0) -> None:
-        self.theta = np.zeros((dim + 1, 1))
-        self._lambda = 0
+    def __init__(self, dim, dim_2 = 1, _lambda = 0) -> None:
+        self.theta = np.zeros((dim + 1, dim_2))
+        self._lambda = _lambda
+    
     def normal_eq(self, features, labels):
         features_bias = np.hstack((features, np.ones((features.shape[0], 1))))
-        self.theta = np.linalg.inv(features_bias.T @ features_bias)@ features_bias.T@labels
+        self.theta = np.linalg.pinv(features_bias.T @ features_bias)@ features_bias.T@labels
 
     def ridge_normal_eq(self, features, labels):
         features_bias = np.hstack((features, np.ones((features.shape[0], 1))))
-        self.theta = np.linalg.inv(features_bias.T @ features_bias + self._lambda * np.identity(features_bias.shape[1])) @ features_bias.T@labels
-    
+        self.theta = np.linalg.inv(features_bias.T @ features_bias + self._lambda * np.identity(features_bias.shape[1])) @ features_bias.T @labels
+
     def MSE(self, features, labels):
         features_bias = np.hstack((features, np.ones((features.shape[0], 1))))
-        return np.sum((features_bias @ self.theta - labels)**2) / features.shape[0]
+        pred = (features_bias @ self.theta).reshape(features_bias.shape[0], self.theta.shape[1])
+        return np.sum((np.array(pred) - np.array(labels).reshape(pred.shape))**2) / (2 * np.array(pred).size)
