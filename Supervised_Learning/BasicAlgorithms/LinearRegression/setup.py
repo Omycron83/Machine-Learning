0b53bdfc2d1ee1776b5170a141a977bbe63d023e
Damian <damian.grunert@gmx.de>
2023-08-19 20:12:25 +0200

    changed setup for linRegr

--- Supervised_Learning/BasicAlgorithms/LinearRegression/setup.py
+++ Supervised_Learning/BasicAlgorithms/LinearRegression/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name = "LinRegression",
       version= "0.1",
       description="Package implementing the linear regression algorithm.",
-      packages=[r"D:\Damian\PC\Python\ML\BasicAlgorithms\LinearRegression"],
+      packages=[r"D:\Damian\PC\Python\ML\Supervised_Learning\BasicAlgorithms\LinearRegression"],
       author="Damian G."
       )
