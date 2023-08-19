c29b2216b6a8eac92c457b68e7874c15185d1ce5
Damian <damian.grunert@gmx.de>
2023-08-19 19:06:40 +0200

    Changed visualization and lin regr for Bruecken

--- Supervised_Learning/BasicAlgorithms/LinearRegression/LinRegr.py
+++ Supervised_Learning/BasicAlgorithms/LinearRegression/LinRegr.py
@@ -10,12 +10,45 @@ class linear_regression:
 
     def ridge_normal_eq(self, features, labels):
         features_bias = np.hstack((features, np.ones((features.shape[0], 1))))
-        self.theta = np.linalg.inv(features_bias.T @ features_bias + self._lambda * np.identity(features_bias.shape[1])) @ features_bias.T @labels
+        reg_matrix = np.identity(features_bias.shape[1])
+        #We need to make sure to not regularize the bias weight, which is done by setting its entry to 0
+        reg_matrix[features_bias.shape[1] - 1, features_bias.shape[1] - 1] = 0
+        self.theta = np.linalg.inv(features_bias.T @ features_bias + self._lambda * reg_matrix) @ features_bias.T @labels
 
     def predict(self, features):
         features_bias = np.hstack((features, np.ones((features.shape[0], 1))))
         return features_bias @ self.theta
+    
     def MSE(self, features, labels):
         features_bias = np.hstack((features, np.ones((features.shape[0], 1))))
         pred = (features_bias @ self.theta).reshape(features_bias.shape[0], self.theta.shape[1])
         return np.sum((np.array(pred) - np.array(labels).reshape(pred.shape))**2) / (2 * np.array(pred).size)
+    
+class polynomial_regression(linear_regression):
+    def __init__(self, dim, dim_2=1, _lambda=0, degree = 1) -> None:
+        self.degree = degree
+        super().__init__(dim * degree, dim_2, _lambda)
+
+    def polynomialize(self, features):
+        return np.hstack(features ** np.arange(1, self.degree + 1)[:, None, None])
+    
+    def normal_eq(self, features, labels):
+        return super().normal_eq(self.polynomialize(features), labels)
+    
+    def ridge_normal_eq(self, features, labels):
+        return super().ridge_normal_eq(self.polynomialize(features), labels)
+    
+    def MSE(self, features, labels):
+        return super().MSE(self.polynomialize(features), labels)
+
+    def predict(self, features):
+        return super().predict(self.polynomialize(features))
+"""
+x = np.arange(9).reshape(3,3)
+print(x)
+y = np.arange(3)
+
+g = polynomial_regression(3, 1, degree=12, _lambda = 0)
+g.ridge_normal_eq(x, y)
+print(g.predict(x), y)
+"""
--- Supervised_Learning/Projects/Brücken/model_visualization.py
+++ Supervised_Learning/Projects/Brücken/model_visualization.py
@@ -1,7 +1,7 @@
 #Final model parameters were:
-lin_opt = [0]
-nn_opt = [453, 0.48377941566444677, 117, 0.0006685591228068569, 0.01999257672168958]
-xgboost_opt = [0.0, 0.04491353111733209, 5, 100, 0.7761690258201821, 10, 5.0, 4.424087360747103]
+lin_opt = [1.2776441026927368]
+nn_opt = [555, 0.5119633527495826, 47, 0.0005015370459087969, 5.0]
+xgboost_opt = [1.2373453011910522, 0.01, 6, 1096, 0.8544992034696949, 1, 0.8009271586748941, 1.4147922102986796]
 
 import xgboost
 import NN
@@ -17,6 +17,9 @@ labels = data["EIGENFREQ_ALT_STUFE_5"].to_numpy()
 labels = labels.reshape((labels.shape[0], 1))
 train_data = data.to_numpy()
 train_data = np.delete(train_data, 1, 1)
+
+data_unknown = pd.read_csv("D:\Damian\PC\Python\ML\Supervised_Learning\Projects\Brücken\DatensatzTraining_Va.csv")
+data_unknown = data.to_numpy()
 
 #Training the linear model with the optimal hyperparameters, visualizing
 print("Starting visualization linear:")
@@ -60,25 +63,22 @@ axis[2].set_ylim(0, data["EIGENFREQ_ALT_STUFE_5"].max())
 axis[2].add_line(line)
 plt.show()
 
-#Saving the values (including overfitting):
+#Saving the values:
 print("Starting saving linear:")
 lin_model = LinRegr.linear_regression(train_data.shape[1], _lambda = lin_opt[0])
 lin_model.ridge_normal_eq(train_data, labels)
-pred_linregr = lin_model.predict(train_data)
-print(pred_linregr[:25].T)
+pred_linregr = lin_model.predict(data_unknown)
 np.savetxt("D:\Damian\PC\Python\ML\Supervised_Learning\Projects\Brücken\LinRegr.csv", pred_linregr, delimiter=",")
 
 print("Starting saving nn:")
 nn_model = NN.cont_feedforward_nn(train_data.shape[1], [nn_opt[0]], NN.ReLU, NN.ReLUDeriv, NN.output, NN.MSE_out_deriv, 1)
-nn_model.adam_iterated(train_data, labels, NN.MSE, dropout= [nn_opt[1]], batchsize = nn_opt[2], alpha = nn_opt[3], _lambda = nn_opt[4], iterations = 1200)
+nn_model.adam_iterated(train_data, labels, NN.MSE, dropout= [nn_opt[1]], batchsize = nn_opt[2], alpha = nn_opt[3], _lambda = nn_opt[4], iterations = 250)
 nn_model.forward_propagation(train_data, labels, NN.MSE)
-pred_nn = nn_model.output_layer()
-print(pred_nn[:25].T)
+pred_nn = nn_model.output_layer(data_unknown)
 np.savetxt("D:\Damian\PC\Python\ML\Supervised_Learning\Projects\Brücken\NN.csv", pred_nn, delimiter=",")
 
 print("Starting saving xgboost:")
 xgboost_model = xgboost.XGBRegressor(gamma = xgboost_opt[0], learning_rate = xgboost_opt[1], max_depth = xgboost_opt[2], n_estimators = xgboost_opt[3], n_jobs = 16, objective = 'reg:squarederror', subsample = xgboost_opt[4], scale_pos_weight = 0, reg_alpha = xgboost_opt[6], reg_lambda = xgboost_opt[7], min_child_weight = xgboost_opt[5])
 xgboost_model.fit(train_data, labels)
-pred_xgboost = xgboost_model.predict(train_data)
-print(pred_xgboost[:25].T)
+pred_xgboost = xgboost_model.predict(data_unknown)
 np.savetxt("D:\Damian\PC\Python\ML\Supervised_Learning\Projects\Brücken\XGBoost.csv", pred_xgboost, delimiter=",")
