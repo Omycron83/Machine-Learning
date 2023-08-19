92b9af191e56f8e7c88cdb8bc303daeb6643b887
Damian <damian.grunert@gmx.de>
2023-08-19 20:15:37 +0200

    changed model_vis for polynomial regression

--- Supervised_Learning/Projects/Brücken/model_visualization.py
+++ Supervised_Learning/Projects/Brücken/model_visualization.py
@@ -14,18 +14,18 @@ import numpy as np
 data = pd.read_csv("D:\Damian\PC\Python\ML\Supervised_Learning\Projects\Brücken\DatensatzTraining_Va.csv")
 data["BETA_HT_Q_DEB"] = data["BETA_HT_Q_DEB"].fillna(data["BETA_HT_Q_DEB"].mean())
 labels = data["EIGENFREQ_ALT_STUFE_5"].to_numpy()
-labels = labels.reshape((labels.shape[0], 1))
-train_data = data.to_numpy()
+labels = labels.reshape((labels.shape[0], 1)).astype('float64')
+train_data = data.to_numpy().astype('float64')
 train_data = np.delete(train_data, 1, 1)
 
 data_unknown = pd.read_csv("D:\Damian\PC\Python\ML\Supervised_Learning\Projects\Brücken\DatensatzTraining_Va.csv")
-data_unknown = data.to_numpy()
+data_unknown = data.to_numpy().astype('float64')
 data_id = data_unknown[:, 0]
 data_unknown = np.delete(data_unknown, 0, 1)
 
 #Training the linear model with the optimal hyperparameters, visualizing
 print("Starting visualization linear:")
-lin_model = LinRegr.linear_regression(train_data.shape[1], _lambda = lin_opt[0])
+lin_model = LinRegr.polynomial_regression(train_data.shape[1], _lambda = lin_opt[1], degree = lin_opt[0])
 lin_model.ridge_normal_eq(train_data[:int(train_data.shape[0] * 0.8)], labels[:int(train_data.shape[0] * 0.8)])
 figure, axis = plt.subplots(3, 1)
 axis[0].scatter(labels[int(train_data.shape[0] * 0.8):], lin_model.predict(train_data[int(train_data.shape[0] * 0.8):]))
@@ -67,7 +67,7 @@ plt.show()
 
 #Saving the values:
 print("Starting saving linear:")
-lin_model = LinRegr.linear_regression(train_data.shape[1], _lambda = lin_opt[0])
+lin_model = LinRegr.polynomial_regression(train_data.shape[1], _lambda = lin_opt[1], degree = lin_opt[0])
 lin_model.ridge_normal_eq(train_data, labels)
 pred_linregr = lin_model.predict(data_unknown)
 np.savetxt("D:\Damian\PC\Python\ML\Supervised_Learning\Projects\Brücken\LinRegr.csv", np.vstack((data_id,  pred_linregr )), delimiter=",")
