f28202f193853b66e75c43b3652d71484dcf20b7
Damian <damian.grunert@gmx.de>
2024-02-22 21:01:47 +0100

    Working on

--- RL/Projects/BLL/ODT.py
+++ RL/Projects/BLL/ODT.py
@@ -1,1 +1,20 @@
-D
+import transformer
+import torch
+
+class ReplayBuffer:
+    def __init__(self) -> None:
+        pass
+
+class ODT:
+    def __init__(self) -> None:
+        pass
+    
+    def pred(self, R, s, a, t):
+        pos_embedding = embed_t(t)
+        
+
+    def train_online(self):
+        pass
+
+    def train_offline(self):
+        pass
